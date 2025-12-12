7126d80d18b7166f4eb744fc273d58926c53fe1b
valeriafz <valeria.frunza33@gmail.com>
2025-12-12 23:25:30 +0200

    feat: add db primary replication for rumour and communication service

--- db/init_communication_primary.sh
+++ db/init_communication_primary.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 set -e
 
-echo "Creating replicator user..."
+echo "Creating replicator user for communication service..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD '$PG_REPLICATION_PASSWORD';
     SELECT pg_create_physical_replication_slot('replica_1_slot');
