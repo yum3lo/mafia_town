f8f3e1af46f435d53b2c29dcbdf60130e08af9bf
nelldino <garbuznelly@gmail.com>
2025-12-13 01:50:25 +0200

    add replicas for town and character service

---
+++ db/init_character_primary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Creating replicator user..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD '$POSTGRES_REPLICATION_PASSWORD';
+    SELECT pg_create_physical_replication_slot('replica_1_slot');
+    SELECT pg_create_physical_replication_slot('replica_2_slot');
+EOSQL
+
+echo "Configuring pg_hba.conf for replication..."
+echo "host replication replicator 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
---
+++ db/init_town_primary.sh
@@ -0,0 +1,11 @@
+set -e
+
+echo "Creating replicator user..."
+psql -v ON_ERROR_STOP=1 --username "$PG_TOWN_USER" --dbname "$PG_TOWN_DB" <<-EOSQL
+    CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD '$POSTGRES_REPLICATION_PASSWORD';
+    SELECT pg_create_physical_replication_slot('replica_1_slot');
+    SELECT pg_create_physical_replication_slot('replica_2_slot');
+EOSQL
+
+echo "Configuring pg_hba.conf for replication..."
+echo "host replication replicator 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
